44220265b56a1715b570eec5a7e90ee9a4258eeb
Advaita <vedanta3000@gmail.com>
2024-02-20 13:35:44 +0530

    fixed frontend

--- src/comp/pages/login.js
+++ src/comp/pages/login.js
@@ -1,7 +1,7 @@
 // Login.js
 import React, {useState} from "react";
 
-import { Link } from "react-router-dom";
+import { Link , useNavigate} from "react-router-dom";
 import '../style.css';
 import email_img from '../../resources/email.png';
 import password_img from '../../resources/password.png';
@@ -13,12 +13,17 @@ export default function Login() {
 
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const navigate = useNavigate();
 
     const handleSubmit = async (e) => {
         e.preventDefault();
         try {
-        const response = await axios.post('http://localhost:5555/profile/login', { email, password });
-        console.log('Login successful:', response.data);
+            const response = await axios.post('http://localhost:5555/profile/login', { email, password },{withCredentials: true,});
+            console.log('Login successful:', response.data);
+            document.cookie =response.cookie
+            if(response.data.user){
+                navigate("/");
+            }   
         } catch (error) {
         console.error('Login failed:', error);
     }}
