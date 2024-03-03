4e9ea2acc6c69344a729fced0c3b1bbf00df9c98
Advaita <vedanta3000@gmail.com>
2024-03-03 23:26:47 +0530

    added vercel link

--- backend/index.js
+++ backend/index.js
@@ -37,7 +37,7 @@ setInterval(updateLeetcodePeriodically,1000 * 60 * 60 * 24);
 app.use(express.json());
 
 app.use(cors({
-    origin: ['http://localhost:3000', 'http://localhost:8000'],
+    origin: ['http://localhost:3000', 'http://localhost:8000','https://code-hub-eight.vercel.app'],
     credentials: true, 
   }));
 
