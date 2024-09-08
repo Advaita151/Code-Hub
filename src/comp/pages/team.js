aa7bccabb0a5e06361dd9e5bb95e002e2acf4d93
Aditya Shukla <aditya.shukla019r@gmail.com>
2024-09-09 01:28:24 +0530

    bugs fixed

--- src/comp/pages/team.js
+++ src/comp/pages/team.js
@@ -2,7 +2,6 @@ import "../style.css";
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
-
 import UserInitials from "../othercomponents/userInitials.js";
 import axios from "axios";
 import img1 from "../../resources/adi.jpeg";
@@ -14,14 +13,11 @@ function Team() {
 
   const [menuOpen, setMenuOpen] = useState(false);
 
- 
-
   const navStyles = {
     backgroundColor: "rgba(0, 0, 0, 1)",
     padding: "10px",
     position: "sticky",
     top: 0,
-
     width: "100%",
     zIndex: 1000,
     transition: "color 0.3s ease",
@@ -86,7 +82,7 @@ function Team() {
   const handleMenuToggle = () => {
     setMenuOpen(!menuOpen);
   };
-  let message = `CodeHub, envisioned and developed by two BTech  students, is a dynamic platform designed for college students. It offers a live leaderboard showcasing rankings across various coding platforms, fostering healthy competition and inspiring students to excel. Additionally, it features competition reminders and an  practice arena (currently under dev) , enhancing coding skills and promoting a vibrant learning community.`;
+  let message = `CodeHub is an innovative platform crafted by two BTech students, tailored specifically for college students. It provides a real-time leaderboard that highlights top performers across multiple coding platforms, encouraging healthy competition and motivating students to reach their full potential. The platform also includes features such as competition reminders and an evolving practice arena (currently under development), aimed at boosting coding skills and fostering a dynamic and engaged learning community.`;
   return (
     <div style={mainStyles}>
       {/* Navbar */}
@@ -119,14 +115,15 @@ function Team() {
           </a>
         </div>
         <div style={profileCircleStyles}>
-          {userName === 'Profile'?(
-          <Link to="/login">
-            <UserInitials name={userName} />
-          </Link>
-          ):(
-          <Link to="/profile">
-            <UserInitials name={userName} />
-          </Link>)}
+          {userName === 'Profile' ? (
+            <Link to="/login">
+              <UserInitials name={userName} />
+            </Link>
+          ) : (
+            <Link to="/profile">
+              <UserInitials name={userName} />
+            </Link>
+          )}
         </div>
       </div>
 
@@ -136,6 +133,51 @@ function Team() {
           <p className="section-subtitle">{message}</p>
         </div>
         <div className="container-new">
+          {/* Swapped positions */}
+          <div className="col-sm-6 col-md-4 container-header2">
+            <div className="team-item">
+              <img src={img1} className="team-img" alt="aditya" />
+              <h3> Aditya Shukla</h3>
+              <div className="team-info">
+                <p>MERN Stack Developer</p>
+                <p>
+                  Enthusiastic Full Stack Developer with a strong
+                  foundation in C++ and MERN Stack. Eager to tackle complex web
+                  development projects and create seamless, user-friendly
+                  experiences. Passionate about honing coding skills through
+                  competitive programming challenges.
+                </p>
+                <ul className="team-icon">
+                  <li>
+                    <a href="" className="twitter">
+                      <i className="fa fa-twitter"></i>
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      href="https://www.instagram.com/who.is.adi"
+                      className="instagram"
+                    >
+                      <i className="fa fa-instagram"></i>
+                    </a>
+                  </li>
+                  <li>
+                    <a href="https://github.com/adityasays" className="github">
+                      <i className="fa fa-github"></i>
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      href="https://www.linkedin.com/in/adityashukla190503/"
+                      className="linkedin"
+                    >
+                      <i className="fa fa-linkedin"></i>
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
           <div className="col-sm-6 col-md-4 container-header2">
             <div className="team-item">
               <img src={img2} className="team-img" alt="advaita" />
@@ -143,12 +185,11 @@ function Team() {
               <div className="team-info">
                 <p>MERN Stack Developer</p>
                 <p>
-                  {" "}
                   Full Stack Developer with a passion for creating efficient and
-                  user-friendly web applications and implementing cutting edge
+                  user-friendly web applications and implementing cutting-edge
                   AI/ML solutions. Proficient in both front-end and back-end
                   technologies. With a solid understanding of MERN stack and
-                  database like firebase.
+                  databases like Firebase.
                 </p>
                 <ul className="team-icon">
                   <li>
@@ -175,51 +216,6 @@ function Team() {
                   <li>
                     <a
                       href="https://www.linkedin.com/in/advaita-vedanta-1b9340202/"
-                      className="linkedin"
-                    >
-                      <i className="fa fa-linkedin"></i>
-                    </a>
-                  </li>
-                </ul>
-              </div>
-            </div>
-          </div>
-          <div className="col-sm-6 col-md-4 container-header2">
-            <div className="team-item">
-              <img src={img1} className="team-img" alt="aditya" />
-              <h3> Aditya Shukla</h3>
-              <div className="team-info">
-                <p>MERN Stack Developer</p>
-                <p>
-                  {" "}
-                  Enthusiastic emerging Full Stack Developer with a strong
-                  foundation in C++ and MERN Stack . Eager to tackle complex web
-                  development projects and create seamless, user-friendly
-                  experiences. Passionate about honing coding skills through
-                  competitive programming challenges
-                </p>
-                <ul className="team-icon">
-                  <li>
-                    <a href="" className="twitter">
-                      <i className="fa fa-twitter"></i>
-                    </a>
-                  </li>
-                  <li>
-                    <a
-                      href="https://www.instagram.com/who.is.adi"
-                      className="instagram"
-                    >
-                      <i className="fa fa-instagram"></i>
-                    </a>
-                  </li>
-                  <li>
-                    <a href="https://github.com/adityasays" className="github">
-                      <i className="fa fa-github"></i>
-                    </a>
-                  </li>
-                  <li>
-                    <a
-                      href="https://www.linkedin.com/in/adityashukla190503/"
                       className="linkedin"
                     >
                       <i className="fa fa-linkedin"></i>
